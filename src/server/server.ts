f6332904a75e02eb18fa29940f2f35397a2c9d30
martiiian <jkramorevx@gmail.com>
2021-08-31 23:20:19 +0400

    Add ts support for server

---
+++ src/server/server.ts
@@ -0,0 +1,29 @@
+const Websocket = require('ws')
+const wsServer = new Websocket.Server({ port: 9000 })
+// eslint-disable-next-line no-console
+console.log('server started on localhost:9000')
+wsServer.on('connection', onConnect)
+
+function onConnect(wsClient) {
+  // eslint-disable-next-line no-console
+  console.log('connection success')
+  setInterval(() => {
+    const someComplexObject = {
+      id: 'someuniqueid123',
+      text: `some biba text ${Math.random()}`,
+      date: new Date()
+    }
+    const messageToClient = JSON.stringify(someComplexObject)
+    wsClient.send(messageToClient)
+  }, 1000)
+
+  wsClient.on('message', function(message) {
+    // eslint-disable-next-line no-console
+    console.log('message received', message)
+  })
+
+  wsClient.on('close', function() {
+    // eslint-disable-next-line no-console
+    console.log('connection closed')
+  })
+}
