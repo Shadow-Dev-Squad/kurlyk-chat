9e1f663fd627362ee70a2915c48fd96a4b0cac69
martiiian <jkramorevx@gmail.com>
2021-09-01 11:07:07 +0400

    Add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+
+  extends: ['plugin:@typescript-eslint/recommended'],
+
+  rules: {
+    'no-extra-semi': 'off',
+    semi: 'off',
+  },
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,28 +1,26 @@
 const Websocket = require('ws')
+
 const wsServer = new Websocket.Server({ port: 9000 })
-// eslint-disable-next-line no-console
-console.log('server started on localhost:9000')
 wsServer.on('connection', onConnect)
+console.log('server started on localhost:9000')
 
 function onConnect(wsClient) {
-  // eslint-disable-next-line no-console
-  console.log('connection success')
   setInterval(() => {
     const someComplexObject = {
       id: 'someuniqueid123',
       text: `some biba text ${Math.random()}`,
-      date: new Date()
+      date: new Date(),
     }
     const messageToClient = JSON.stringify(someComplexObject)
     wsClient.send(messageToClient)
   }, 1000)
 
-  wsClient.on('message', function(message) {
+  wsClient.on('message', function (message) {
     // eslint-disable-next-line no-console
     console.log('message received', message)
   })
 
-  wsClient.on('close', function() {
+  wsClient.on('close', function () {
     // eslint-disable-next-line no-console
     console.log('connection closed')
   })
